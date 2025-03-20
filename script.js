e46f0f102ec061319c8f3c7806071aca879e7054
ezy1218 <ziyoda110@icloud.com>
2025-03-20 10:17:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+alert('sizda hozir random son chiqadi')
+let chislo = Math.ceil(Math.random() *100) 
+alert('random orqali chiqgan son:' + chislo)
+alert(chislo + ' soni bu juft son')
+
+alert('sizda hozir random son chiqadi')
+let toq = Math.floor(Math.random() *100) 
+alert('random  son :' + toq)
+alert(toq + ' :son bu toq son')
+
+let raqam = prompt('raqam kiriting')
+let ikiraqam = prompt(' ikkinchi raqam kiriting')
+
+let a = 'ОК'
+let b = 'ОТМЕНА'
+alert(`agar ${a}  knopkasini bossangiz siz kiritgan raqamlar bir biriga qoshiladi`)
+alert(`agar ${b}  knopkasini bossangiz siz kiritgan raqamlar bir biriga qoshiladi`)
+
+let num1 = 8;
+let num2 = 12;
+let sum = num1 + num2;
+alert(num1 + " + " + num2 + " = " + sum);
+
+let ikiiraqam = prompt(' ikkinchi raqam kiriting')
+alert(`agar ${a}  knopkasini bossangiz siz kiritgan raqamlar bir biriga qoshiladi`)
+alert(`agar ${b}  knopkasini bossangiz siz kiritgan raqamlar bir biridan ayriladi`)
+
+let num3 = 0;
+let num4 = 15;
+let summ = num3 + num4;
+alert(num3 + " - " + num4 + " = " + summ);
+
+
+alert('0 soni 15 sonidan kichik bolganligi uchun ayirish amali bajarilmadi')
+
+
+let roqam = prompt(' raqam raqam kiriting')
+let ikiroq = prompt('ikkinchi raqam kiriting')
+alert(`agar ${a}  knopkasini bossangiz siz kiritgan raqamlar bir biriga qoshiladi`)
+alert(`agar ${b}  knopkasini bossangiz siz kiritgan raqamlar bir biridan ayriladi`)
+
+let num5 = 15;
+let num6 = 20;
+let sunm = num5 + num6;
+alert(num5 + " - " + num6 + " = " + sunm);
+
+alert('15 soni 20 sonidan kichik bolganligi uchun ayirish amali bajarilmadi')
